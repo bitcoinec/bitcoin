teddywest32/bitcoinclassic

Add blocksizeacceptlimitbytes command-line option
Also change DEFAULT_BLOCK_ACCEPT_SIZE constant from a float to an int32_t

src/policy/policy.h
@@ -28,7 +28,7 @@ static const unsigned int MAX_STANDARD_TX_SIGOPS = MAX_BLOCK_SIGOPS/5;
 /** Default for -maxmempool, maximum megabytes of mempool memory usage */
 static const unsigned int DEFAULT_MAX_MEMPOOL_SIZE = 300;
 /** Default for -blocksizeacceptlimit */
-static const float DEFAULT_BLOCK_ACCEPT_SIZE = 3.7;
+static const int32_t DEFAULT_BLOCK_ACCEPT_SIZE = 3.7e6;
 
 /**
  * Standard script verification flags that standard transactions will comply

bitcoinec/bitcoin

src/policy/policy.h
@@ -29,7 +29,7 @@ static const unsigned int MAX_STANDARD_TX_SIGOPS_COST = MAX_BLOCK_SIGOPS_COST/5;
 /** Default for -maxmempool, maximum megabytes of mempool memory usage */
 static const unsigned int DEFAULT_MAX_MEMPOOL_SIZE = 300;
 /** Default for -blocksizeacceptlimit */
-static const float DEFAULT_BLOCK_ACCEPT_SIZE = 3.7;
+static const float DEFAULT_BLOCK_ACCEPT_SIZE = 3.7e6;
 /** Default for -incrementalrelayfee, which sets the minimum feerate increase for mempool limiting or BIP 125 replacement **/
 static const unsigned int DEFAULT_INCREMENTAL_RELAY_FEE = 1000;
 /** Default for -bytespersigop */